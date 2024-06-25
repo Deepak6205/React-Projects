d28573ebb7d922ea33f26781fcfd39ca4ea16a16
deepak kumar <deepak7292832956@gmail.com>
2024-06-25 17:38:37 +0530

    project -3 star rating

--- react-interview-projects/src/App.js
+++ react-interview-projects/src/App.js
@@ -2,14 +2,18 @@
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StartRating from './components/star rating';
 function App() {
   return (
     <div className="App">
       {/*Accordian Component*/}
-      <Accordian/>
+      {/* <Accordian/> */}
 
       {/* Random Color component */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
+
+      {/* star rating component */}
+      <StartRating noOfStarts={10}/>
     </div>
   );
 }
